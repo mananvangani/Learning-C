816fca1464b5489139bf87e0e2f39de630433874
Manan <vanganimanan84@gmail.com>
2024-09-02 12:56:13 +0530

    farcel calc pt 2 push

--- initial/far-cel converter pt2.c
+++ initial/far-cel converter pt2.c
@@ -12,7 +12,7 @@ step=20;
 fahr=lower;
 while(fahr<=upper){
     celsius=(5.0/9.0)*(fahr-32.0);
-    printf("%3.2f\t%4.3f\n",fahr,celsius);
+    printf("%3.2f\t|%4.3f\n\n",fahr,celsius);
     fahr=fahr+step;}
 
 }
