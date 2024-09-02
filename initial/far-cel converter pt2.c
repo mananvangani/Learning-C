e84ddd523ecbf0fd0c30c6945a04cfa76ab2a45a
Manan <vanganimanan84@gmail.com>
2024-09-02 11:55:36 +0530

    far cel converter pt 2

---
+++ initial/far-cel converter pt2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+main()
+
+{ float lower, upper, step;
+float fahr, celsius;
+
+lower=0;
+upper=300;
+step=20;
+
+fahr=lower;
+while(fahr<=upper){
+    celsius=(5.0/9.0)*(fahr-32.0);
+    printf("%3.2f\t%4.3f\n",fahr,celsius);
+    fahr=fahr+step;}
+
+}
